d215f25c4c9f6bd80dda81392f047ab504260135
Acer <Acer@DESKTOP-LHN9MF2>
2025-02-06 13:59:22 +0530

    pollscmtest class added

---
+++ simplejenkins/src/test/java/demo/PollScm.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScm 
+{
+	@Test
+	public void pollscmtest()
+	{
+		Reporter.log("pollscmtest class executed");
+	}
+
+}
